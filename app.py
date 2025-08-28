744cdae96867393118e1c98fe4e820f144ad3401
OccupationalYou <dydd32008@gmail.com>
2025-08-28 18:02:19 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from base import create_db, drop_db, Session
+from models.product import Product
+from models.user import User
+from models.associations import user_products
+# from routes.products import products_bp
+
+
+create_db()
+app = Flask("__name__")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
