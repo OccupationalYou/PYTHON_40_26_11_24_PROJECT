03e87b41bc25b392cc3f4a97ba6ac3cc2a16e70a
vladyslava-martyniuk <143871786+vladyslava-martyniuk@users.noreply.github.com>
2025-08-30 16:26:18 +0300

    Update associations.py

--- models/associations.py
+++ models/associations.py
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-from sqlalchemy import Column, Integer, Table, ForeignKey
 
-from base import Base
-
-
-user_products = Table(
-    "user_product",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("users.id")),
-    Column("product_id", Integer, ForeignKey("products.id")),
-=======
-from sqlalchemy import Column, Integer, Table, ForeignKey
-
-from base import Base
-
-
-user_products = Table(
-    "user_product",
-    Base.metadata,
-    Column("user_id", Integer, ForeignKey("users.id")),
-    Column("product_id", Integer, ForeignKey("products.id")),
->>>>>>> Stashed changes
-)
-=======
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from base import Base
 
@@ -33,4 +8,4 @@ user_products = Table(
     Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
     Column("product_id", Integer, ForeignKey("products.id"), primary_key=True),
 )
->>>>>>> e1c03cece7392e360da7ab2d96d83aa0e9cccd3e
+
