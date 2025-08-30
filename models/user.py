a5f8f59fb01132e98bdc4af6592f17f45d51a00e
OccupationalYou <dydd32008@gmail.com>
2025-08-30 13:59:47 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,20 +1,16 @@
-
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
-
+from sqlalchemy.orm import Mapped, mapped_column, relationship 
+from sqlalchemy import Integer, String
 from base import Base
-
+from models.associations import user_products
+from models.product import Product
 
 class User(Base):
-    __tablename__ = 'users'
+    tablename = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
-    name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    email: Mapped[str] = mapped_column(String(128), nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
 
     products: Mapped[list["Product"]] = relationship(
-        "Product", secondary="user_product", back_populates="users"
+        "Product", secondary=user_products, back_populates="users"
     )
-
-
