5ae1b2f0fffde9186b87ba0bb47cc0140fc77200
OccupationalYou <dydd32008@gmail.com>
2025-09-02 17:06:25 +0300

    Add files via upload

---
+++ models/associations.py
@@ -0,0 +1,10 @@
+
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from base import Base
+
+user_products = Table(
+    "user_products",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("product_id", Integer, ForeignKey("products.id"))
+)
